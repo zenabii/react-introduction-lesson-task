a42cc92b952f03cc4b4aab8d1e1a611d676f08d3
zenabii <maria.turtles@outlook.com>
2021-11-06 00:00:31 +0100

    tests

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Heading from './components/Heading';
+import Paragraph from './components/Paragraph';
+
+function App() {
+  return (
+    <div className="container">    
+    <Heading />
+    <Paragraph />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Paragraph.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Paragraph() {
+    return <p>new component paragraph</p>
+}
+
+export default Paragraph;
